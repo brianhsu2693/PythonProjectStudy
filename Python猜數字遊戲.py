6d735c211c5409491413f2c1afec8eb6eae20af4
Brian Hsu <brianhsu2693@gmail.com>
2023-04-10 17:32:10 +0800

    增加用戶字定義功能

--- Python猜數字遊戲.py
+++ Python猜數字遊戲.py
@@ -1,10 +1,15 @@
 import random  # 匯入random模組
-r = random.randint(1, 100)  # 從1~10中隨機產生一個數字
+start = input('請填入猜數字範圍(開頭)：')  # 讓用戶填寫開頭至結尾的範圍
+end = input('請填入猜數字範圍(結尾)：')
+start = int(start)  # 字串轉換為整數
+end = int(end)
+
+r = random.randint(start, end)  # 從指定範圍中隨機產生一個數字
 count = 0  # 初始化count (count等於猜題次數)
 
 while True:  # 執行無限迴圈
     count += 1  # count = count + 1 (每執行一次都會+1，用來計算共執行幾次)
-    num = input('請猜 1 ~ 100 的數字：')  # 讓用戶輸入數字
+    num = input(f'請猜 {start} ~ {end} 的數字：')  # 讓用戶輸入數字
     num = int(num)  # 字串型態轉換為整數
     if num == r:
         print('太厲害了，你猜對了！')

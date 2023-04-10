f23b122a78f5fc07b52cc0ea09ba89a9ba61b708
Brian Hsu <brianhsu2693@gmail.com>
2023-04-10 15:06:50 +0800

    內文修改

--- Brian's study.py
+++ Brian's study.py
@@ -1,7 +1,13 @@
-x = 5
-while x > 1:
-    print('重複執行')
-    print('測試無限迴圈')
-
-    x = x - 1  # 設立跑一次迴圈所要執行的條件
-print('我逃出迴圈了~~~~~~~')
+password = '12345'  # 設定密碼
+user_try = 3  # 設定輸入次數
+while True:
+    pwd = input('請輸入密碼：')
+    if pwd == password:
+        print('密碼正確')
+        break  # 密碼正確，跳出迴圈
+    else:
+        user_try = user_try - 1  # 計算使用次數
+        print('輸入錯誤，你還有', user_try, '次機會')
+        if user_try == 0:
+            print('輸入太多次錯誤，帳號已被鎖定！')
+            break  # 輸入太多次錯誤，跳出迴圈

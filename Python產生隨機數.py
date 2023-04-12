9f9c9e65c7543b0d927a653272b3802532cf2b64
Brian Hsu <brianhsu2693@gmail.com>
2023-04-12 23:11:06 +0800

    內文修改

--- Python產生隨機數.py
+++ Python產生隨機數.py
@@ -1,6 +1,5 @@
 import random  # 匯入random模組
 
-range(5)  # [0, 1, 2, 3, 4]
 for i in range(5):  # 宣告1個i變數，採用for loop去一筆一筆讀取range(5)清單資料
     r = random.randint(1, 100)  # 宣告變數r，隨機產生範圍1~100中的數字
     print(f'這是第 {i + 1} 次產生隨機數 {r}。')  # 列印次數與range清單範圍有直接關係

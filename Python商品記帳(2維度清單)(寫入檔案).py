599ea8766fee1a5a87b056eb872eb821921524cb
Brian Hsu <brianhsu2693@gmail.com>
2023-04-13 14:09:23 +0800

    內文修改

--- Python商品記帳(2維度清單)(寫入檔案).py
+++ Python商品記帳(2維度清單)(寫入檔案).py
@@ -8,8 +8,16 @@ while True:  # 使用無限迴圈來執行
     p = [name, price]  # 建立1個小清單p，把用戶輸入的name跟price裝進p小清單
     projects.append(p)  # 把p小清單裝到projects大清單
 
-print(f'輸入清單中的項目有：{projects}')
+print('--------- 分隔線 ---------')
 print(f'清單中總共有{len(projects)}個商品')
+print(f'輸入清單中的項目有：{projects}')
 print('--------- 分隔線 ---------')
 print(f'第一個商品的名稱為：{projects [0][0]}')  # 把projects大清單中的第1格內的第1格拿出來
 print(f'第一個商品的價格為：{projects [0][1]}')  # 把projects大清單中的第1格內的第2格拿出來
+print('--------- 分隔線 ---------')
+for p in projects:  # 宣告一個變數p，使用for loop將projects內的資料一個個取出
+    print(f'{p[0]}價格是{p[1]}元')  # 列印出每個取出的小清單內的數值
+
+with open('products', 'w') as f:
+    for p in projects:
+        f.write(p[0] + ', ' + p[1] + '\n')

83dac5b2aea21832ad0659a4d8ba35aa1f83f168
Brian Hsu <brianhsu2693@gmail.com>
2023-04-19 10:48:39 +0800

    程式碼與排版優化

--- Brian's study.py
+++ Brian's study.py
@@ -1,12 +1,5 @@
-# 讀取檔案
-def read_file():  # 建立read_file function
-    lines = []  # 建立一個空清單
-    with open('chat.txt', 'r', encoding='utf-8') as f:  # 使用with open開啟檔案，並指定檔案名為f
-                                                        # 因為檔案有中文，所以要指定讀取的編碼格式為utf-8
+text1 = '本科，碩士，博士，應屆畢業生皆歡迎'
+txt_list = text1.split('，')
 
-        for line in f:  # 使用line變數一行行的讀取f檔案
-            lines.append(line)  # 將line讀取出來的檔案裝進lines清單
-    return lines  #記得！ 要回傳出來！
-
-lines = read_file()
-print(lines)
+print(f'原字串：{text1}')
+print(f'切割字串後：{txt_list} __變成清單了')

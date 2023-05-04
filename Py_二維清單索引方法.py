10d24121124e1c6c46bd14e6892a0a2a82e7b2b5
Brian Hsu <brianhsu2693@gmail.com>
2023-05-04 13:12:35 +0800

    程式實做第1版

---
+++ Py_二維清單索引方法.py
@@ -0,0 +1,17 @@
+row1 = ['⬜️','️⬜️','️⬜️']
+row2 = ['⬜️','️⬜️','️⬜️']
+row3 = ['⬜️','️⬜️','️⬜️']
+map = [row1, row2, row3]
+print(map)
+print(f'{row1}\n{row2}\n{row3}')
+position = input('Where do you want to put ? ')  # 用戶輸入2位數字代表要選擇的位置
+
+x = int(position[0])  # 索引[0]找出用戶輸入的第1個數字，並轉換成整數，然後存為變數x
+                      # x代表索引第幾個子清單
+y = int(position[1])  # 索引[1]找出用戶輸入的第2個數字，並轉換成整數，然後存為變數y
+                      # y代表所以x子清單中的位置
+map[x - 1][y - 1] = 'X'  # 索引2維清單的位置，並將該位置內容更換為'X'字串
+                         # map[0][]第一格放入變數x-1代表著縱向第2格
+                         # map[][1]第二格放入變數x-1代表著橫向第1格
+print(map)
+print(f"{row1}\n{row2}\n{row3}")
--- completed_project/Py_聚餐付款抽籤器.py
+++ completed_project/Py_聚餐付款抽籤器.py
@@ -2,12 +2,13 @@
 import random
 # Split string method
 names_string = input("Give me everybody's names, separated by a comma. ")
-names = names_string.split(", ")
+names = names_string.split(", ")  # 使用.split()function來切割字串並存為清單
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
-names_len = len(names)
-payer_num = random.randint(0, names_len - 1)
-payer = names[payer_num]
+names_len = len(names)  # 計算清單長度(清單內的數量)
+payer_num = random.randint(0, names_len - 1)  # 使用.randint()function來來選擇隨機抽出的範圍
+                                              # ()中的0為起始，names_len-1為清單結尾
+payer = names[payer_num]  # 將抽出來的數字帶入names清單做索引，並存入payer變數作為抽出來的付款人
 
 print(f'{payer} is going to buy the meal today! ')
